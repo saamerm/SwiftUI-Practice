1372a749a533b983a3f71f0f19ab548d65b1d0b4
Saamer Mansoor <i@saamer.me>
2019-08-26 15:04:23 -0400

    Basic App

---
+++ Official Tutorials/KashmiriLandmarks/KashmiriLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KashmiriLandmarks
+//
+//  Created by Saamer Mansoor on 2019-08-26.
+//  Copyright © 2019 Saamer Mansoor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
